69d6a974b4eaf43536656a46f523534b5cbf9181
glencu <glencek@gmail.com>
2016-09-25 21:54:01 +0200

    Files added

---
+++ CurrentLogger/OrnoManager.cpp
@@ -0,0 +1,18 @@
+/*
+ * OrnoManager.cpp
+ *
+ *  Created on: 25 wrz 2016
+ *      Author: Bobasek
+ */
+
+#include "OrnoManager.h"
+
+OrnoManager::OrnoManager() {
+	// TODO Auto-generated constructor stub
+
+}
+
+OrnoManager::~OrnoManager() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ CurrentLogger/OrnoManager.h
@@ -0,0 +1,17 @@
+/*
+ * OrnoManager.h
+ *
+ *  Created on: 25 wrz 2016
+ *      Author: Bobasek
+ */
+
+#ifndef ORNOMANAGER_H_
+#define ORNOMANAGER_H_
+
+class OrnoManager {
+public:
+	OrnoManager();
+	virtual ~OrnoManager();
+};
+
+#endif /* ORNOMANAGER_H_ */
