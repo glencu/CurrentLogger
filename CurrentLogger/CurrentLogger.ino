e8ae6e1508ced6a36f54385e606b564155c436f6
glencu <glencek@gmail.com>
2016-09-25 21:41:38 +0200

    eth added

--- CurrentLogger/CurrentLogger.ino
+++ CurrentLogger/CurrentLogger.ino
@@ -2,12 +2,42 @@
 #include <SD.h>
 #include <Wire.h>
 #include <DS3231.h>
+#include <EtherCard.h>
+
 
 #include "RS485.h"
 #include "OrnoReadHoldingRegistersCommand.h"
 #include "OrnoReadHoldingRegistersResponse.h"
 #include "OrnoHelper.h"
 #include "SDLogger.h"
+
+#define POWER_METER_IDX (13)
+
+// ethernet interface mac address, must be unique on the LAN
+static byte mymac[] = { 0x74,0x69,0x69,0x2D,0x30,0x31 };
+
+byte Ethernet::buffer[700];
+
+const char website[] PROGMEM = "192.168.8.6:8080";
+const static uint8_t ip[] = {192,168,137,100};
+const static uint8_t gw[] = {192,168,137,1};
+const static uint8_t dns[] = {192,168,137,1};
+
+static void my_callback (byte status, word off, word len) {
+  int status_index = -1;
+  int ok_index = -1;
+  Serial.println(">>>");
+  Ethernet::buffer[off+300] = 0;
+
+
+  Serial.print((const char*) Ethernet::buffer + off);
+
+  if ( strstr((const char*) Ethernet::buffer + off , ": \"OK\"") != NULL )
+	  Serial.println("OK");
+  else
+	  Serial.println("Error");
+}
+
 
 
 #define rxPin 0 // Our Serial RX pin connected to RO- Receiver Output Pin on Max485- we receive on
@@ -25,7 +55,8 @@ OrnoReadHoldingRegistersResponse readResponse;
 char logBuffer[BUFF_LEN];
 char dateBuf[DATE_LEN];
 unsigned char responseBuffer[OrnoReadHoldingRegistersResponse::RESPONSE_LENGHT];
-SDLogger *sdlogger;
+SDLogger *sdPowerLogger;
+SDLogger *sdErrorLogger;
 
 void setup() {
 
@@ -51,7 +82,8 @@ void setup() {
     return;
   }
   Serial.println("initialization done.");
-  sdlogger = new SDLogger();
+  sdPowerLogger = new SDLogger();
+  sdErrorLogger = new SDLogger("Error.txt");
   delay(1000);
   rs485.Init();
   //No Serial.println from this line!!
@@ -59,17 +91,35 @@ void setup() {
   readCommand.setDataAdress(0x00);
   readCommand.setNumOfRegisters(0x0010);
 
+
+  if (ether.begin(sizeof Ethernet::buffer, mymac) == 0)
+     Serial.println(F("Failed to access Ethernet controller"));
+  if(!ether.staticSetup(ip, gw, dns))
+     Serial.println(F("static failed"));
+
+  ether.printIp("IP:  ", ether.myip);
+  ether.printIp("GW:  ", ether.gwip);
+  ether.printIp("DNS: ", ether.dnsip);
+
+  ether.parseIp(ether.hisip, "192.168.8.6");
+  ether.printIp("SRV: ", ether.hisip);
+  Serial.print("Port: ");
+  ether.hisport = 8080;
+  Serial.println(ether.hisport);
+
   delay(1000);
 
 }
 
-void loop() {
+void loop()
+{
+  ether.packetLoop(ether.packetReceive());
 
   unsigned char* command = readCommand.getCommand();
-
   rs485.Send(command);
   delete command;
 
+  bool goodFrameReceived = false;
   dt = clock.getDateTime();
   memset(dateBuf, 0 , DATE_LEN);
   sprintf(dateBuf,"%d-%d-%d %d:%d:%d ",dt.year,dt.month,dt.day,dt.hour,dt.minute,dt.second);
@@ -86,24 +136,36 @@ void loop() {
 
       if ( evaluatedCrc == receivedFrameCrc )
       {
-
-    	  sprintf(logBuffer,"%s Volts: %d Current %d.%d Actual Power %d.%2d Kwh %lu.%2lu\n" ,dateBuf,readResponse.getVolt() ,
-    			  readResponse.getCurrent().value, readResponse.getCurrent().fraction ,
-				  readResponse.getActualPower().value,readResponse.getActualPower().fraction,
-				  readResponse.getKWh().value,readResponse.getKWh().fraction);
-
+    	  goodFrameReceived = true;
       }
       else
       {
     	  sprintf(logBuffer," %s Received frame bad CRC\n", dateBuf );
-
       }
   }
   else
   {
 	  sprintf(logBuffer,"%s Error when receiving data from module\n" ,dateBuf);
   }
-  sdlogger->LogToFile(logBuffer);
+
+  if ( goodFrameReceived == true )
+  {
+	  sprintf(logBuffer,"%s Volts: %d Current %d.%d Actual Power %d.%2d Kwh %lu.%2lu\n" ,dateBuf,readResponse.getVolt() ,
+	    			  readResponse.getCurrent().value, readResponse.getCurrent().fraction ,
+					  readResponse.getActualPower().value,readResponse.getActualPower().fraction,
+					  readResponse.getKWh().value,readResponse.getKWh().fraction);
+	  sdPowerLogger->LogToFile(logBuffer);
+
+	  char buffer[100];
+	  memset(buffer, 0 ,100);
+	  sprintf(buffer,"/json.htm?type=command&param=udevice&idx=%d&nvalue=0&svalue=%d",POWER_METER_IDX, readResponse.getKWh().value);
+	  ether.browseUrl("",buffer, website, my_callback);
+
+  }
+  else
+  {
+	  sdErrorLogger->LogToFile(logBuffer);
+  }
 
 
   delay(1000); //for test
